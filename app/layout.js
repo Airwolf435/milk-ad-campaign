c19d097d17af5eeaa6dc85ee17849a13a4f91dac
John Keenan <jgknomad@gmail.com>
2024-02-18 22:11:48 -0500

    Remove header from global layout

--- app/layout.js
+++ app/layout.js
@@ -12,8 +12,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <Header/>
-        {/* <Header></Header> */}
         {children}
         <Footer/>
         </body>
