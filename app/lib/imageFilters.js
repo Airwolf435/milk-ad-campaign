0edb71e8045b17839448385c8b36512fd5b00100
John Keenan <jgknomad@gmail.com>
2024-02-22 23:36:02 -0500

    Base stickers and filters for game complete

--- app/lib/imageFilters.js
+++ app/lib/imageFilters.js
@@ -14,19 +14,21 @@ export function noirFilm(pixels) {
     return pixels;
 }
 
-//This is a filter from stuarts example, it will be replaced and is here soley for testing purposes.
-export function rgbSplit(pixels) {
+export function blackHot(pixels) {
     for (let i = 0; i < pixels.data.length; i += 4) {
-        pixels.data[i + 0] = pixels.data[i + 100]; // RED
-        pixels.data[i + 1] = pixels.data[i + 1]; // GREEN
-        pixels.data[i + 2] = pixels.data[i + 202]; // Blue
+        let intensity =  Math.floor((pixels.data[i + 0] + pixels.data[i + 1]  + pixels.data[i + 2]) / 3);
+        pixels.data[i + 0] = 255 - intensity;
+        pixels.data[i + 1] = 255 - intensity;
+        pixels.data[i + 2] = 255 - intensity;
     }
     return pixels;
 }
 
-export function miamiVibe(width, height){
-    let gradient = this.createLibearGradient(0,0, width, height);
-    gradient.addColorStop(1, "##d9802646");
+export function miamiVibe(target){
+    let width = document.querySelector(`.${target}`).width;
+    let height = document.querySelector(`.${target}`).height;
+    let gradient = this.createLinearGradient(0,0, width, height);
+    gradient.addColorStop(0, "#d9802646");
     gradient.addColorStop(1, "#9500b146");
     this.fillStyle = gradient;
     this.fillRect(0,0, width, height);
--- app/ui/contestGame/ContestGame.jsx
+++ app/ui/contestGame/ContestGame.jsx
@@ -2,17 +2,95 @@
 
 import { useEffect, useState } from "react"
 import styles from "./contestGame.module.css"
-import { noirFilm, rgbSplit } from "@/app/lib/imageFilters";
+import { blackHot, miamiVibe, noirFilm, rgbSplit, tvDistortion } from "@/app/lib/imageFilters";
+import { createImage } from "@/app/lib/helpers";
 
 // Credit to Stuart Sackler, their example on using canvas 2d contexts to manipulate images from the webcam were translated from vanilla JS into something usable within react.
 export default function ContestGame(){
-    const [activeFilter, setActiveFilter] = useState(()=>noirFilm);
+    const [activeFilter, setActiveFilter] = useState();
     const [imageHistory, setImageHistory] = useState([]);
     const [imageBase, setImageBase] = useState([]);
+    const [videoPlayer, setVideoPlayer] = useState();
     const [context, setContext] = useState();
     const [updateInterval, setUpdateInterval] = useState();
-
     const [videoStream, setVideoStream] = useState();
+    const [stickers, setStickers] = useState([]);
+    const [activeSticker, setActiveSticker] = useState();
+
+    const filters = {
+        "noirFilm": noirFilm,
+        "blackHot": blackHot,
+        "miamiVibe": miamiVibe.bind(context, styles.contestWindow)
+    };
+
+    function startFrames(filter){
+        clearInterval(updateInterval);
+        let canvas = document.querySelector(`.${styles.contestWindow}`);
+        setUpdateInterval(setInterval(()=>{
+            let playerElement = document.querySelector(`.${styles.hiddenPlayer}`);
+            context.drawImage(playerElement, 0, 0, canvas.width, canvas.height);
+            context.fillStyle = "transparent";
+            context.fillRect(0,0, canvas.width, canvas.height);
+            for(let sticker of stickers){
+                context.drawImage(sticker.image, sticker.x, sticker.y);
+            }
+            let currentFrame = context.getImageData(0,0, canvas.width, canvas.height);
+            // context.drawImage(createImage("/assets/imgs/stickers/coolCow.png"), 25, 25);
+            
+            let newFrame = currentFrame;
+            if(filter !== undefined){
+                if(filter.next !== undefined){
+                    // newFrame = filter.next(currentFrame).value;
+                    console.log(filter.next(currentFrame).value);
+                }else{
+                    newFrame = filter(currentFrame);
+                }
+            }
+            context.putImageData(newFrame, 0, 0);
+        }, 50));
+    }
+
+
+    function handleFilterButton(e){
+        let filterName = e.target.value;
+        if(activeFilter === filterName){
+            setActiveFilter(undefined);
+        }else{
+            setActiveFilter(filterName);
+        }
+    }
+
+    function handleStickerClick(src){
+        setActiveSticker(src);
+    }
+
+    function hadnleCanvasClick(e){
+        if(activeSticker){
+            let targetRect = e.target.getBoundingClientRect()
+            let xPercent = Math.round(((e.pageX - targetRect.x) / targetRect.width) * 100) / 100;
+            let yPercent = Math.round((((e.pageY - targetRect.y) - window.scrollY) / targetRect.height) * 100) / 100;
+            setStickers([
+                ...stickers,
+                {
+                    image: createImage(activeSticker),
+                    x: 1920 * xPercent,
+                    y: 1080 * yPercent
+                }
+            ]);
+        }
+    }
+
+    function handleTakePhoto(){
+        let historyStack = document.querySelector("#historyStack");
+        let canvasSnap = document.querySelector(`.${styles.contestWindow}`).toDataURL("images/png");
+
+        let newPhoto = document.createElement("a");
+        newPhoto.setAttribute("download", true);
+        newPhoto.href = canvasSnap;
+        newPhoto.innerHTML = `<img src=${canvasSnap} alt="Photo taken as part of the milk ad campaign"/>`;
+        historyStack.appendChild(newPhoto);
+    }
+    
 
     useEffect(()=>{
         navigator.mediaDevices.getUserMedia({
@@ -22,42 +100,52 @@ export default function ContestGame(){
             setVideoStream(newVideoStream);
         });
         setContext(document.querySelector(`.${styles.contestWindow}`).getContext("2d"));
-    }, []);
+    });
+
+    useEffect(()=>{
+        if(videoStream && context && !videoPlayer){
+            let hiddenPlayer = document.querySelector(`.${styles.hiddenPlayer}`);
+            hiddenPlayer.srcObject = videoStream;
+            setVideoPlayer(hiddenPlayer.play());
+            startFrames();
+            filters["miamiVibe"] = miamiVibe.bind(context, styles.contestWindow);
+        }
+    }, [videoStream, context]);
 
     useEffect(()=>{
         if(videoStream && context){
-            console.log("Bois, we gotem.");
-            let videoPlayer = document.querySelector(`.${styles.hiddenPlayer}`)
-            videoPlayer.srcObject = videoStream;
-            videoPlayer.play();
-            if(updateInterval){
-                clearInterval(updateInterval);
-            }
-            console.log("Lets Draw")
-            let canvas = document.querySelector(`.${styles.contestWindow}`);
-            setUpdateInterval(setInterval(()=>{
-                context.drawImage(videoPlayer, 0, 0, canvas.width, canvas.height);
-                console.log(context.getImageData(0,0, canvas.width, canvas.height));
-                let currentFrame = context.getImageData(0,0, canvas.width, canvas.height);
-                let newframe = activeFilter(currentFrame)
-                context.putImageData(newframe, 0, 0);
-            }, 100));
+            startFrames(activeFilter ? filters[activeFilter] : undefined);
         }
-    }, [videoStream, context]);
+    },[activeFilter, stickers])
 
     return (
         <section>
             <aside>
                 <h2>Filters</h2>
+                <button value={"noirFilm"} onMouseDown={handleFilterButton}>Noir Filter</button>
+                <button value={"prism"} onMouseDown={handleFilterButton}>Prism</button>
+                <button value={"miamiVibe"} onMouseDown={handleFilterButton}>Miami Vibe</button>
             </aside>
             <div>
-                <canvas className={styles.contestWindow} width="1920" height="1080"></canvas>
+                <canvas className={styles.contestWindow} width="1920" height="1080" onClick={hadnleCanvasClick}></canvas>
                 <video src="" className={styles.hiddenPlayer}></video>
-                <button>Camera Snap</button>
+                <button onClick={handleTakePhoto}>Camera Snap</button>
             </div>
             <aside>
                 <h2>Stickers</h2>
+                <button onClick={()=>{return handleStickerClick("/assets/imgs/stickers/coolCow.png")}}>
+                    <img src="/assets/imgs/stickers/coolCow.svg"  alt="" className={styles.sticker}/>
+                </button>
+                <button onClick={()=>{return handleStickerClick("/assets/imgs/stickers/happyCow.png")}}>
+                    <img src="/assets/imgs/stickers/happyCow.svg"  alt="" className={styles.sticker}/>
+                </button>
+                <button onClick={()=>{return handleStickerClick("/assets/imgs/stickers/loveCow.png")}}>
+                    <img src="/assets/imgs/stickers/loveCow.svg"  alt="" className={styles.sticker}/>
+                </button>
             </aside>
+            <section id="historyStack">
+
+            </section>
         </section>
     )
 }
